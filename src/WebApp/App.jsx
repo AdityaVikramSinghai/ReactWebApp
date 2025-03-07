01709c4324c3ea7a821999961a0e773329e5001e
Aditya Vikram Singhai <adityavikramsinghai@Mac.Home>
2025-03-07 02:06:59 +0000

    React Routing

--- src/WebApp/App.jsx
+++ src/WebApp/App.jsx
@@ -1,16 +1,53 @@
 import React, { useEffect, useState } from "react";
 import './App.css';
+import { BrowserRouter, createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Home from "../component/Home";
+import About from "../component/About";
+import Dashboard from "../component/Dashboard";
+import NavBar from "../component/NavBar";
+import ParamComp from "../component/ParamComp";
 
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <div>
+        <NavBar />
+        <Home />
+      </div>
+    },
+    {
+      path: "/about",
+      element: <div>
+        <NavBar />
+        <About />
+      </div>
+    },
+    {
+      path: "/dashboard",
+      element: <div>
+        <NavBar />
+        <Dashboard />
+      </div>
+    },
+    {
+      path: "/user/:id",
+      element: <div>
+        <NavBar />
+        <ParamComp />
+      </div>
+    }
+  ]
+);
 
 export default function App() {
-  
+
   useEffect(() => {
   }, []);
-  
+
   return (
     <div className="App">
-       <header className="App-header">
-      </header> 
+      <RouterProvider router={router} />
     </div>
   );
 }
