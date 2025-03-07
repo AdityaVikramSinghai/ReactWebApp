2db2eae16b55961dc9b8ef7223bc893fe4cb8268
Aditya Vikram Singhai <adityavikramsinghai@Mac.Home>
2025-03-07 00:25:44 +0000

    React App init

---
+++ src/WebApp/App.jsx
@@ -0,0 +1,16 @@
+import React, { useEffect, useState } from "react";
+import './App.css';
+
+
+export default function App() {
+  
+  useEffect(() => {
+  }, []);
+  
+  return (
+    <div className="App">
+       <header className="App-header">
+      </header> 
+    </div>
+  );
+}
